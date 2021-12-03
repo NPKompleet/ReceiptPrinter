d8449b8c72b284f2df6ae44ae9feb78f4a5852ad
Philip Okonkwo <philipcokonkwo@yahoo.com>
2021-12-03 20:09:50 +0100

    Initial project scaffold

---
+++ src/main/java/com/npkompleet/tax/SalesTax.java
@@ -0,0 +1,12 @@
+package com.npkompleet.tax;
+
+public class SalesTax {
+
+	public static void main(String[] args) {
+		// TODO Create input files
+		// TODO Create Tax class
+		// TODO Create Item class
+
+	}
+
+}
