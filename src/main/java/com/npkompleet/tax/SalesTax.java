885003f2fb282b51631e09edd6999e14f0c18dd9
Philip Okonkwo <philipcokonkwo@yahoo.com>
2021-12-03 20:25:44 +0100

    Created input files

--- src/main/java/com/npkompleet/tax/SalesTax.java
+++ src/main/java/com/npkompleet/tax/SalesTax.java
@@ -3,7 +3,6 @@ package com.npkompleet.tax;
 public class SalesTax {
 
 	public static void main(String[] args) {
-		// TODO Create input files
 		// TODO Create Tax class
 		// TODO Create Item class
 
