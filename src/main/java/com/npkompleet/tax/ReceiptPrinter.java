39744a561c3960eb915ee479358991e661697dcb
Philip Okonkwo <philipcokonkwo@yahoo.com>
2021-12-03 20:44:46 +0100

    Renamed SalesTax class to ReceiptPrinter

--- src/main/java/com/npkompleet/tax/ReceiptPrinter.java
+++ src/main/java/com/npkompleet/tax/ReceiptPrinter.java
@@ -1,8 +1,9 @@
 package com.npkompleet.tax;
 
-public class SalesTax {
+public class ReceiptPrinter {
 
 	public static void main(String[] args) {
+		// TODO Add README with assumptions
 		// TODO Create Tax class
 		// TODO Create Item class
 
