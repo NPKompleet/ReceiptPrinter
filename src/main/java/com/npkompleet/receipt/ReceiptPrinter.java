7ceeeb0fce080c4c4436598fb9286a3bf622cccd
Philip Okonkwo <philipcokonkwo@yahoo.com>
2021-12-04 12:18:24 +0100

    Implemented reading input file

--- src/main/java/com/npkompleet/receipt/ReceiptPrinter.java
+++ src/main/java/com/npkompleet/receipt/ReceiptPrinter.java
@@ -1,11 +1,41 @@
 package com.npkompleet.receipt;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ReceiptPrinter {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ReceiptPrinter.class);
 
 	public static void main(String[] args) {
 		// TODO Create Tax class
 		// TODO Create Item class
-		// TODO Create input file reader
+
+		for (String arg : args) {
+			try (FileInputStream fstream = new FileInputStream(arg);
+					BufferedReader reader = new BufferedReader(new InputStreamReader(fstream));) {
+				String line;
+				while ((line = reader.readLine()) != null) {
+					if (InputValidator.isValidInput(line)) {
+						System.out.println(line);
+					} else {
+						System.out.println("Not a valid input file format");
+						break;
+					}
+				}
+			} catch (FileNotFoundException e) {
+				LOGGER.error(e.getMessage());
+			} catch (IOException e) {
+				LOGGER.error(e.getMessage());
+			}
+		}
+
 		// TODO Create input parser
 	}
 
